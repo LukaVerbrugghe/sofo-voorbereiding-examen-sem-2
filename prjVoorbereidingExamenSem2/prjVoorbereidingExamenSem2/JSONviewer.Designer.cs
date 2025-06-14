5e844cfac62a535d2e6e4586e85a0b4a8a1e0287
LukaVerbrugghe <lotrdetailsys@gmail.com>
2025-06-14 09:45:40 +0200

    Design

---
+++ prjVoorbereidingExamenSem2/prjVoorbereidingExamenSem2/JSONviewer.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace prjVoorbereidingExamenSem2
+{
+    partial class JSONviewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsbJSONData = new System.Windows.Forms.ListBox();
+            this.btnLoadJson = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lsbJSONData
+            // 
+            this.lsbJSONData.FormattingEnabled = true;
+            this.lsbJSONData.Location = new System.Drawing.Point(13, 13);
+            this.lsbJSONData.Name = "lsbJSONData";
+            this.lsbJSONData.Size = new System.Drawing.Size(433, 407);
+            this.lsbJSONData.TabIndex = 0;
+            // 
+            // btnLoadJson
+            // 
+            this.btnLoadJson.Location = new System.Drawing.Point(453, 13);
+            this.btnLoadJson.Name = "btnLoadJson";
+            this.btnLoadJson.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadJson.TabIndex = 1;
+            this.btnLoadJson.Text = "Load JSON";
+            this.btnLoadJson.UseVisualStyleBackColor = true;
+            // 
+            // JSONviewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 450);
+            this.Controls.Add(this.btnLoadJson);
+            this.Controls.Add(this.lsbJSONData);
+            this.Name = "JSONviewer";
+            this.Text = "JSONviewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lsbJSONData;
+        private System.Windows.Forms.Button btnLoadJson;
+    }
+}
---
+++ prjVoorbereidingExamenSem2/prjVoorbereidingExamenSem2/frmLogin.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace prjVoorbereidingExamenSem2
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLoginGebruikersnaam = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLoginWachtwoord = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSignupGebruikersnaam = new System.Windows.Forms.TextBox();
+            this.txtSignupWachtwoord = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSignupBevestigWachtwoord = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(166, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Log in";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(144, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 73);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Sign up";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Gebruikersnaam:";
+            // 
+            // txtLoginGebruikersnaam
+            // 
+            this.txtLoginGebruikersnaam.Location = new System.Drawing.Point(179, 90);
+            this.txtLoginGebruikersnaam.Name = "txtLoginGebruikersnaam";
+            this.txtLoginGebruikersnaam.Size = new System.Drawing.Size(241, 20);
+            this.txtLoginGebruikersnaam.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Wachtwoord:";
+            // 
+            // txtLoginWachtwoord
+            // 
+            this.txtLoginWachtwoord.Location = new System.Drawing.Point(179, 126);
+            this.txtLoginWachtwoord.Name = "txtLoginWachtwoord";
+            this.txtLoginWachtwoord.Size = new System.Drawing.Size(241, 20);
+            this.txtLoginWachtwoord.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 299);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Gebruikersnaam:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(86, 335);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Wachtwoord:";
+            // 
+            // txtSignupGebruikersnaam
+            // 
+            this.txtSignupGebruikersnaam.Location = new System.Drawing.Point(179, 296);
+            this.txtSignupGebruikersnaam.Name = "txtSignupGebruikersnaam";
+            this.txtSignupGebruikersnaam.Size = new System.Drawing.Size(241, 20);
+            this.txtSignupGebruikersnaam.TabIndex = 2;
+            // 
+            // txtSignupWachtwoord
+            // 
+            this.txtSignupWachtwoord.Location = new System.Drawing.Point(179, 332);
+            this.txtSignupWachtwoord.Name = "txtSignupWachtwoord";
+            this.txtSignupWachtwoord.Size = new System.Drawing.Size(241, 20);
+            this.txtSignupWachtwoord.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(86, 371);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Bevestig wachtwoord:";
+            // 
+            // txtSignupBevestigWachtwoord
+            // 
+            this.txtSignupBevestigWachtwoord.Location = new System.Drawing.Point(204, 368);
+            this.txtSignupBevestigWachtwoord.Name = "txtSignupBevestigWachtwoord";
+            this.txtSignupBevestigWachtwoord.Size = new System.Drawing.Size(216, 20);
+            this.txtSignupBevestigWachtwoord.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(89, 157);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(331, 49);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "LOG IN";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnSignup
+            // 
+            this.btnSignup.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignup.Location = new System.Drawing.Point(89, 397);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(331, 49);
+            this.btnSignup.TabIndex = 3;
+            this.btnSignup.Text = "SIGN UP";
+            this.btnSignup.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 458);
+            this.Controls.Add(this.btnSignup);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtSignupBevestigWachtwoord);
+            this.Controls.Add(this.txtSignupWachtwoord);
+            this.Controls.Add(this.txtLoginWachtwoord);
+            this.Controls.Add(this.txtSignupGebruikersnaam);
+            this.Controls.Add(this.txtLoginGebruikersnaam);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLoginGebruikersnaam;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLoginWachtwoord;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSignupGebruikersnaam;
+        private System.Windows.Forms.TextBox txtSignupWachtwoord;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSignupBevestigWachtwoord;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnSignup;
+    }
+}
+
